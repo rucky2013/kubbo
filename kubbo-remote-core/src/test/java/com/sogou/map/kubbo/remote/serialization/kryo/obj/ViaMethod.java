dda0e3ec1f29865d23d7706bbf5bf3ee2ab119ab
刘福良 <liufuliang@sogou-inc.com>
2017-02-09 18:12:05 +0800

    Add missing files

---
+++ kubbo-remote-core/src/test/java/com/sogou/map/kubbo/remote/serialization/kryo/obj/ViaMethod.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.sogou.map.kubbo.remote.serialization.kryo.obj;
+
+/**
+ * @author liufuliang
+ *
+ */
+public enum ViaMethod{  
+    VIA,    //经过
+    AVOID   //避让
+};
---
+++ kubbo-rpc-core/src/main/java/com/sogou/map/kubbo/rpc/ServiceInvoker.java
@@ -0,0 +1,10 @@
+package com.sogou.map.kubbo.rpc;
+
+/**
+ * ServiceInvoker
+ * tag interface
+ * @author liufuliang
+ */
+public interface ServiceInvoker<T> extends Invoker<T> {
+
+}
