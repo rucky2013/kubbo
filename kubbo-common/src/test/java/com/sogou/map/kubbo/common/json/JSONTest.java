26af0229896be1fe717a52526d83ac89f35234c6
刘福良 <liufuliang@sogou-inc.com>
2017-05-18 18:06:16 +0800

    Allow NaN number

--- kubbo-common/src/test/java/com/sogou/map/kubbo/common/json/JSONTest.java
+++ kubbo-common/src/test/java/com/sogou/map/kubbo/common/json/JSONTest.java
@@ -15,6 +15,7 @@ import com.sogou.map.kubbo.common.json.JSONObject;
  *
  */
 public class JSONTest {
+    
     @Test
     public void testJSONParse() throws JSONException{
         JSONObject obj = new JSONObject("{\"uid\": \"1000443103746\",\"caption\": \"清华大学\",\"category\": \"学校科研\",\"subCategory\": \"知名大学\",\"alias\": \"清华,清华大学本部,清华总部,清华总校区,清华大学校区,THU校区,清华本校区,清华大学本校区,清华大学主校区,清华大学总部,清华大学总校区,清华校区,THU,清华本部,清华主校区,清华学校,国立清华大学,西南联合大学\",\"phone\": \"010-62782165;010-62785001\",\"address\": \"北京市海淀区双清路30号\",\"poiDesc\": \"\",\"province\": \"北京市\",\"city\": \"北京市\",\"county\": \"海淀区\",\"geometry\": {\"type\": \"POINT\",\"coordinate\": [12949561,4837546]}}");
