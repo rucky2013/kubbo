45d1bce991bbabf5726d11fc9fbfd1094660f9c9
刘福良 <liufuliang@sogou-inc.com>
2017-06-21 15:15:08 +0800

    compatible with trace address with path

--- kubbo-trace-zipkin/src/main/java/com/sogou/map/kubbo/trace/zipkin/ZipkinTracerFactory.java
+++ kubbo-trace-zipkin/src/main/java/com/sogou/map/kubbo/trace/zipkin/ZipkinTracerFactory.java
@@ -22,8 +22,8 @@ public class ZipkinTracerFactory implements TracerFactory{
     @Override
     public Tracing create(URL address){
         String application = address.getParameter(Constants.APPLICATION_KEY, NetUtils.getHostAddress());        
-        
-        Sender sender = URLConnectionSender.create("http://" + address.getAddress() + "/api/v1/spans");
+
+        Sender sender = URLConnectionSender.create(address.setProtocol("http").toIdentityString() + "/api/v1/spans");
         
         AsyncReporter<zipkin.Span> reporter = AsyncReporter.create(sender);
         
